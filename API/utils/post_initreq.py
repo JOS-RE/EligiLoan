3ad1a71b2daeb77db89cda3f673aadf65c947747
JOS-RE <gopidi.joshith@gmail.com>
2023-01-09 12:41:01 +0530

    Added secrets to the streamlit cloud app

--- API/utils/post_initreq.py
+++ API/utils/post_initreq.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import os
 import requests
 from dotenv import load_dotenv
@@ -10,14 +11,14 @@ def init_req(phonenum, templateType):
     body = {
         "vuaId": f"{phonenum}@dashboard-aa-uat",
         "templateType": templateType,
-        "trackingId": os.environ.get("trackingId"),
+        "trackingId": st.secrets["trackingId"],
         "redirectionUrl": "https://devfolio.co/bobworld-hackathon/dashboard"
     }
 
     headers = {
         "Content-Type": "application/json",
         "Accept": "application/json",
-        "API_KEY": os.environ.get("API_KEY")
+        "API_KEY": st.secrets["API_KEY"]
     }
 
     response = requests.post(url, json=body, headers=headers)  
@@ -29,12 +30,12 @@ def fetch_data(phonenum, templateType):
     url=f"{BASE_URL}/consent/data/fetch"
     params={
         "referenceId":init_req(phonenum, templateType)["referenceId"],
-        "trackingId": os.environ.get("trackingId")
+        "trackingId": st.secrets["trackingId"]
     }
     headers = {
         "Content-Type": "application/json",
         "Accept": "application/json",
-        "API_KEY": os.environ.get("API_KEY")
+        "API_KEY": st.secrets["API_KEY"]
     }
     print(params)
     print(headers)
@@ -47,19 +48,19 @@ def fetch_analysed_data(phonenum, templateType):
     url=f"{BASE_URL}/consent/analytics/fetch"
     params={
         "referenceId":init_req(phonenum, templateType),
-        "trackingId": os.environ.get("trackingId")
+        "trackingId": st.secrets["trackingId"]
     }
     headers = {
         "Content-Type": "application/json",
         "Accept": "application/json",
-        "API_KEY": os.environ.get("API_KEY")
+        "API_KEY": st.secrets["API_KEY"]
     }
 
     getanalysedrequest = requests.get(url, params=params, headers=headers)
 
     return getanalysedrequest.json()
 
-req=init_req("9205231677","ONETIME")
+req=init_req("9922700288","ONETIME")
 print(req.json()["redirectionUrl"])
 # req=init_req("9205231677","ONETIME")
 # print(req)
