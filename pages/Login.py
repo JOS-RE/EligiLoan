3ea8a91b3e38a59f8df8f4b4f94ce24c69440e72
JOS-RE <gopidi.joshith@gmail.com>
2023-01-09 12:24:15 +0530

    Redirection link is set in the button

--- pages/Login.py
+++ pages/Login.py
@@ -10,7 +10,20 @@ st.markdown("""
 phone_number = st.text_input("Phone Number")
 template_type = st.selectbox("Template Type", ["ONETIME", "PERIODIC"])
 
-if st.button("Login"):
-    req=init_req(phone_number, template_type)
-    if req.json()["redirectionUrl"]:
-        st.write(req.json()["redirectionUrl"])
+col11, col22, col3 = st.columns(3)
+
+with col11:
+    if st.button("Verify"):
+        req=init_req(phone_number, template_type)
+        if req.json()["redirectionUrl"]:
+            linkk = req.json()["redirectionUrl"]
+        with col22:
+            st.write(f'''
+                        <a target="blank" href="{linkk}">
+                            <button style=" border-color: orange; padding:10px 20px;   background-color: #fa6400f0; color:white;  border: none;  border-radius: .25rem;" >
+                                Go to accept consent
+                            </button>
+                        </a>
+                        ''',
+                        unsafe_allow_html=True
+                    )
